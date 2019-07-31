cfff923ca2c6a432d94a5753257a40e5189477fc
Fraxcelsior <marten_bonnema@hotmail.com>
2019-07-31 13:58:34 +0200

    intial develop commit

--- database/migrations/2019_07_31_114724_create_journals_table.php
+++ database/migrations/2019_07_31_114724_create_journals_table.php
@@ -15,6 +15,7 @@ class CreateJournalsTable extends Migration
     {
         Schema::create('journals', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->array('videos')
             $table->timestamps();
         });
     }
