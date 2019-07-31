5c0dd5ee234bde79775080f7879f5019b3c9e70a
Fraxcelsior <marten_bonnema@hotmail.com>
2019-07-31 17:50:35 +0200

    add basic journal router

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,35 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Journal;
+
+Route::get('journals', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Journal::all();
+});
+ 
+Route::get('journals/{id}', function($id) {
+    return Journal::find($id);
+});
+
+Route::post('journals', function(Request $request) {
+    return Journal::create($request->all);
+});
+
+Route::put('journals/{id}', function(Request $request, $id) {
+    $journal = Journal::findOrFail($id);
+    $journal->update($request->all());
+
+    return $journal;
+});
+
+Route::delete('journals/{id}', function($id) {
+    Journal::find($id)->delete();
+
+    return 204;
+});
+
 
 /*
 |--------------------------------------------------------------------------
